926ed42d0ace8379f2f4e496434235a4ffc33f36
mjofficials <manojmarimuthu1995@gmail.com>
2021-04-12 21:34:32 +0530

    init commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import CartItem from "./CartItem/CartItem";
+import useStyles from "./styles";
+import { Link } from "react-router-dom";
+
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => {
+    return (
+      <Typography variant="subtitle1">
+        You have no items in your shopping cart,{" "}
+        <Link to="/" className={classes.link}>
+          start adding some
+        </Link>
+        !
+      </Typography>
+    );
+  };
+
+  const FilledCart = () => {
+    return (
+      <>
+        <Grid container spacing={3}>
+          {cart.line_items.map((item) => (
+            <Grid item xs={12} sm={4} key={item.id}>
+              <CartItem
+                onUpdateCartQty={handleUpdateCartQty}
+                onRemoveFromCart={handleRemoveFromCart}
+                item={item}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        <div className={classes.cardDetails}>
+          <Typography variant="h4">
+            Subtotal: {cart.subtotal.formatted_with_symbol}
+          </Typography>
+          <div>
+            <Button
+              className={classes.emptyButton}
+              size="large"
+              type="button"
+              variant="contained"
+              color="secondary"
+              onClick={handleEmptyCart}
+            >
+              Empty cart
+            </Button>
+            <Button
+              className={classes.checkOutButton}
+              size="large"
+              type="button"
+              variant="contained"
+              color="primary"
+            >
+              Checkout
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  if (!cart.line_items) return "Loading...";
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your shopping cart
+      </Typography>
+      {cart.line_items_length === 0 ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
